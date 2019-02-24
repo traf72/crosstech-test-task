dfd442ac6f9d0b319865c8b923402a6fcf594400
traf72 <traf72@gmail.com>
2019-02-24 21:08:24 +0300

    Clean code

--- TestTask.Web/ClientApp/src/decorators/ProtectRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ProtectedRoute from '../components/ProtectedRoute';
-
-export default (route, roles, Component) => class ProtectRoute extends React.Component {
-    render() {
-        return <ProtectedRoute path={route.url} exact={route.exact} component={Component} allowedRoles={roles} />;
-    }
-}
